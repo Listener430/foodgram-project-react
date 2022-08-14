7d2e2ad80916c9abf96e569f2072afdfba09816b
Надежда Калмыкова <nklepik@mail.ru>
2022-08-14 18:08:43 +0300

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="fake_key")
+SECRET_KEY = os.getenv("SECRET_KEY", default="fake_ke")
 
 DEBUG = True
 
@@ -67,8 +67,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": os.getenv(
+            "DB_ENGINE", default="django.db.backends.postgresql"
+        ),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
