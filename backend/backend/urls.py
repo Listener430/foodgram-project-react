97f62e312f693b6666005ee31a1e3d10c9f999a2
Надежда Калмыкова <nklepik@mail.ru>
2022-08-14 14:02:37 +0300

    workflow

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("foodgram_api.urls")),
     path("api/", include("djoser.urls")),
-    path("api/", include("djoser.urls.authtoken")),
+    path("api/auth/", include("djoser.urls.authtoken")),
     path(
         "api/docs/",
         TemplateView.as_view(template_name="redoc.html"),
